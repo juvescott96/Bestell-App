b97a9534e2be8055e68d3b72485f161dd818e31b
Dustin Condello <dustincondello@googlemail.com>
2025-03-24 19:43:21 +0100

    change onclick to basket and style

--- scripts/templates.js
+++ scripts/templates.js
@@ -8,8 +8,8 @@ function getDishesContent(indexDishes) {
                 <p>mit ${dish.description}</p>
                 <p class="card-text">${(dish.price).toFixed(2).replace('.', ',')}€</p>
             </div>
-            <button type="button" onclick="addDishToCart('${dish.name}', ${dish.price})">
-             <img class="plus" src="./assets/icons/plus.png" alt="+ click">
+            <button type="button" onclick="addDishToBasket('${dish.name}', ${dish.price})">
+             <img class="icon" src="./assets/icons/plus.png" alt="+ click">
              </button>
         </div>
     `;
@@ -25,10 +25,30 @@ function getDessertContent(indexDessert) {
                 <p>mit ${dessert.description}</p>
                 <p class="card-text">${(dessert.price).toFixed(2).replace('.', ',')}€</p>
             </div>
-            <button type="button" onclick="addDishToCart('${dessert.name}', ${dessert.price})">
-             <img class="plus" src="./assets/icons/plus.png" alt="+ click">
+            <button type="button">
+             <img class="icon" src="./assets/icons/plus.png" alt="+ click">
              </button>
         </div>
     `;
     return dessertContent;
+}
+
+function addDishToBasket(name, price) {
+    let basketRef = document.getElementById('basket');
+    let basketContent = `
+        <div class="basket-card">
+            <p>${name}</p>
+            <div class="basket-card-body-row">
+               <div class="basket-card-body">
+                 <button><img class="icon" src="./assets/icons/minus.png" alt="- button"></button>
+                 <p>Zahl</p>
+                 <button><img class="icon" src="./assets/icons/plus.png" alt="+ button"></button>
+               </div>
+               <p>${(price).toFixed(2).replace('.', ',')}€</p>
+               <button><img class="icon" src="./assets/icons/trash.png" alt="trash"></button>
+            </div>
+            `;
+            
+            
+    basketRef.innerHTML = basketContent;
 }
